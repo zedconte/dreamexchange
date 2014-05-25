104776d60fcb72d50ed85fc8f94e66d28e950553
Raul Ortega <raul.ortega@unosquare.com>
2014-05-24 19:06:46 -0500

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ymdosAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
